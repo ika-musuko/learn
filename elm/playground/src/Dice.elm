5fa37fea859442dafe2b1eae2c1f5a82d502042d
sherwyn <sherwyn.sen@gmail.com>
2019-12-08 23:46:11 -0800

    finish dice example

--- elm/playground/src/Dice.elm
+++ elm/playground/src/Dice.elm
@@ -4,6 +4,8 @@ import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
 import Random
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 
@@ -71,9 +73,86 @@ subscriptions model =
 -- VIEW
 
 
+drawDots : Int -> List (Svg Msg)
+drawDots dieFace =
+    case dieFace of
+        1 ->
+            [ drawDot 50 50 ]
+
+        2 ->
+            [ drawDot 15 15
+            , drawDot 85 85
+            ]
+
+        3 ->
+            [ drawDot 15 15
+            , drawDot 50 50
+            , drawDot 85 85
+            ]
+
+        4 ->
+            [ drawDot 15 15
+            , drawDot 15 85
+            , drawDot 85 15
+            , drawDot 85 85
+            ]
+
+        5 ->
+            [ drawDot 15 15
+            , drawDot 15 85
+            , drawDot 50 50
+            , drawDot 85 15
+            , drawDot 85 85
+            ]
+
+        6 ->
+            [ drawDot 15 15
+            , drawDot 15 85
+            , drawDot 15 50
+            , drawDot 85 50
+            , drawDot 85 15
+            , drawDot 85 85
+            ]
+
+        _ ->
+            []
+
+
+drawDot : Int -> Int -> Svg Msg
+drawDot x y =
+    circle
+        [ cx (String.fromInt x)
+        , cy (String.fromInt y)
+        , r "10"
+        ]
+        []
+
+
+drawDie : Int -> List (Svg Msg)
+drawDie dieFace =
+    [ rect
+        [ width "100"
+        , height "100"
+        , rx "15"
+        , ry "15"
+        , fill "white"
+        ]
+        []
+    ]
+        ++ drawDots dieFace
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text (String.fromInt model.dieFace) ]
-        , button [ onClick Roll ] [ text "Roll" ]
+        [ h1
+            []
+            [ Html.text (String.fromInt model.dieFace) ]
+        , svg
+            [ width "100"
+            , height "100"
+            , viewBox "0 0 120 120"
+            ]
+            (drawDie model.dieFace)
+        , button [ onClick Roll ] [ Html.text "Roll" ]
         ]
